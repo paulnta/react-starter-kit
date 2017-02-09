ca0f6ee72921a9c1bc976c0122231e4d5d80dbd0
paulnta <paul.ntawuruhunga@heig-vd.ch>
2017-02-09 20:38:33 +0100

    removed unnecessary elements

--- src/routes/index.js
+++ src/routes/index.js
@@ -17,12 +17,7 @@ export default {
   // Keep in mind, routes are evaluated in order
   children: [
     require('./home').default,
-    require('./contact').default,
-    require('./login').default,
-    require('./register').default,
     require('./about').default,
-    require('./privacy').default,
-    require('./admin').default,
     require('./test').default,
     // Wildcard routes, e.g. { path: '*', ... } (must go last)
     require('./notFound').default,
